177a4b1a73ff166eb2b50fd9b41ac6ab8405276e
Ulyana Shabunina <66304635+UlyanaShabunina@users.noreply.github.com>
2022-02-26 13:58:46 +0300

    Update Program.cs

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,23 +1,60 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace Task_2
+using System.Text;
+ 
+namespace ConsoleApplication1
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            float R = 0f; 
+            float X = 0f; 
+            float Y = 0f; 
+ 
+            while (R <= 0)
+            {
+                Console.WriteLine("Введите значение R: ");
+                R = Convert.ToSingle(Console.ReadLine());
+            }
+            Console.Write("Введите Х ");
+            X = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите Y ");
+            Y = Convert.ToSingle(Console.ReadLine());
+            if (X >= -R && X <= R && Y <= R && Y >= -R)
+            {
+                if (X <= 0 && Y >= 0 && Math.Pow(X, 2) + Math.Pow(Y, 2) <= Math.Pow(R, 2))//krug
+                {
+                    Console.WriteLine("1Точка с координатами ({0};{1}) попала в закрашенную  область", X.ToString(), Y.ToString());
+                }
+                else if (X >= 0 && Y <= 0 && X <= R && Y >= -R)//treygolnik
+                {
+                    double tx1 = -(Y / 2);
+                    double ty2 = -(X * 2);
+                    double tx3 = (R + Y + 4) / 2;
+                    double ty4 = 2 * (X - (R / 2)) - 4;
+                    if (X <= R / 2 && Y >= -R && X <= tx1 && Y <= ty2)
+                    {
+                        Console.WriteLine("2Точка с координатами ({0};{1}) попала в закрашенную  область", X.ToString(), Y.ToString());
+                    }
+                    else if (X>=(R/2) && tx3>=X && ty4<=Y && Y>=-R)
+                        {
+                            Console.WriteLine("3Точка с координатами ({0};{1}) попала в закрашенную  область", X.ToString(), Y.ToString());
+                        }
+                        else
+                        {
+                            Console.WriteLine("1Точка с координатами ({0};{1}) не попала никуда.", X.ToString(), Y.ToString());
+ 
+                        }
+                }
+            }
+            else
+            {
+                Console.WriteLine("2Точка с координатами ({0};{1}) не попала никуда.", X.ToString(), Y.ToString());
+            }
+            Console.ReadKey();
+ 
         }
     }
 }
